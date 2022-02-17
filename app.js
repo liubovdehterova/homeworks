fdbce9135ac6da2ae06ead641318655722ce327a
liubovdehterova <lubovdegtereva60@gmail.com>
2022-02-17 22:28:20 +0200

    function warm

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-//test 1
+const { info, warm } = require('./utils/logger');
+
+info('It`s info', 'green color');
+warm('It`s warm', 'yellow color');
--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const chalk = require('chalk');
+const log = console.log;
+function info(...args) {
+    log(chalk.green(args));
+}
+function warm(...args) {
+    debugger;
+    log(chalk.yellow(args));
+}
+module.exports = { info, warm };
